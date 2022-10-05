9db4917486bd48c1159e88c8d61863b9b2623a48
Kevin <KevinPho1012@gmail.com>
2022-10-05 15:29:28 -0700

    created server file to deploy

---
+++ server/index.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const router = require("./routes.js");
+const pool = require('../db/postgres.js')
+const morgan = require('morgan')
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use("/crossing", router);
+
+const port = process.env.PORT || 3001;
+
+app.listen((port), () => {
+  console.log(`Server is running on port:${port}`);
+});
+
+/* socket IO stuff */
+const http = require('http').Server(app);
+
+const socketIO = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000' // incoming URL for clients
+  }
+});
+
+socketIO.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('message', (data) => {
+    socketIO.emit('messageResponse', data);
+
+  });
+  socket.on('disconnect', () => {
+    console.log('a user disconnected');
+  });
+});
+
+http.listen(3002, () => {
+  console.log('chat is listening on *:3002');
+});
+
+/* end of socket IO stuff */
