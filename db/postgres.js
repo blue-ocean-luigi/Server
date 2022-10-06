83b6fa467ba8a1b667f7826420dcdf74f28e4000
Brian Pham <phambri97@gmail.com>
2022-10-05 17:04:53 -0700

    Fixing postgres`

--- db/postgres.js
+++ db/postgres.js
@@ -2,6 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: process.env.DB_USER,
+  host: process.env.DB_HOST,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME
 })
